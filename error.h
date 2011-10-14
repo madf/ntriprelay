9244802e391619c643ca2f97bfb58b75d877077a
Maxim Mamontov <faust.madf@gmail.com>
2011-10-14 17:18:37 +0300

    CasterCategory with some errors defined

--- error.h
+++ error.h
@@ -3,12 +3,48 @@
 
 #include <stdexcept>
 
+#include <boost/system/error_code.hpp>
+
 namespace Caster {
+
+enum {
+    success,
+    resolveError,
+    invalidStatus,
+    connectionTimeout
+};
 
 class CasterError : public std::runtime_error {
     public:
         CasterError(const char * msg) : std::runtime_error(msg) {}
         CasterError(const std::string & msg) : std::runtime_error(msg) {}
+};
+
+class CasterCategory : public boost::system::error_category
+{
+    public:
+        const char * name() const { return "Caster"; }
+        std::string  message(int ev) const
+        {
+            switch (ev) {
+                case success:
+                    return "Success";
+                case resolveError:
+                    return "Failed to resolve supplied address";
+                case invalidStatus:
+                    return "Invalid status string";
+                case connectionTimeout:
+                    return "Connection timeout";
+                default:
+                    return "Unknown error";
+            };
+        }
+
+        static CasterCategory & getInstance()
+        {
+            static CasterCategory category;
+            return category;
+        }
 };
 
 }
